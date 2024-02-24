fbafe5c9885573bf018c90a104e1cb9a86e71f07
khe <khj778878@naver.com>
2024-02-24 14:52:22 +0900

    [feat]: Layout 스타일 컴포넌트 작성

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import Header from "../common/Header";
 import Footer from "../common/Footer";
+import styled from "styled-components";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,10 +10,15 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <LayoutStyle>{children}</LayoutStyle>
       <Footer />
     </>
   );
 };
+
+const LayoutStyle = styled.main`
+  width: 100%;
+  margin: 0 auto;
+`;
 
 export default Layout;
