85941780ddd3ca9fa8cdb105000a3df0d8aba140
khe <khj778878@naver.com>
2024-03-14 16:36:35 +0900

    [feat]: tanstack-query의 useInfinityQuery 훅을 이용한 무한 스크롤 기능 추가
    1. useInfinityQuery 커스텀 훅 생성 & 도서 상세 페이지에 적용
    - 페이지 매개변수를 이용하여 다음 페이지가 존재할 경우, '더 많은 도서보기'라는 문구의 버튼이 보이도록 구현
    - 마지막 페이지일 경우, '마지막 페이지'라는 문구의 버튼이 보이도록 수정
    - 추후 이 useInfinityQuery 훅과 intersectionObserver Web API를 이용하여 무한 자동 스크롤 기능으로 변경할 예정

--- src/pages/BooksPage.tsx
+++ src/pages/BooksPage.tsx
@@ -9,9 +9,12 @@ import Empty from "@/components/common/Empty";
 import { FaRegGrinBeamSweat } from "@react-icons/all-files/fa/FaRegGrinBeamSweat";
 import { HiCursorClick } from "@react-icons/all-files/hi/HiCursorClick";
 import Loading from "@/components/common/Loading";
+import { useBooksInfinity } from "@/hooks/useBooksInfinity";
+import Button from "@/components/common/Button";
 
 const BooksPage = () => {
-  const { books, pagination, isEmpty, message, isBooksLoading } = useBooks();
+  const { books, pagination, isEmpty, message, isBooksLoading, fetchNextPage, hasNextPage } =
+    useBooksInfinity();
 
   if (isBooksLoading || !books || !pagination) {
     return <Loading />;
@@ -19,27 +22,37 @@ const BooksPage = () => {
 
   return (
     <BooksStyle>
-      <Title size="large">도서 검색 결과</Title>
+      <div className="main-contents">
+        <Title size="large">도서 검색 결과</Title>
 
-      <div className="filter">
-        <BooksFilter />
-        <BooksViewSwitcher />
+        <div className="filter">
+          <BooksFilter />
+          <BooksViewSwitcher />
+        </div>
+        {isEmpty && (
+          <Empty
+            icon={<FaRegGrinBeamSweat />}
+            linkIcon={<HiCursorClick />}
+            title={message as string}
+            link="/"
+            linkMsg="메인 화면으로 가기"
+          />
+        )}
+        {!isEmpty && <BooksList books={books} />}
       </div>
-      {isEmpty && (
-        <Empty
-          icon={<FaRegGrinBeamSweat />}
-          linkIcon={<HiCursorClick />}
-          title={message as string}
-          link="/"
-          linkMsg="메인 화면으로 가기"
-        />
-      )}
-      {!isEmpty && (
-        <>
-          <BooksList books={books} />
-          <Page pagination={pagination} />
-        </>
-      )}
+
+      <div className="more">
+        <Button
+          size="medium"
+          scheme="primary"
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage}
+        >
+          {hasNextPage ? "더 많은 도서보기" : "마지막 페이지"}
+        </Button>
+      </div>
+
+      {/* {!isEmpty && <Page pagination={pagination} />} */}
     </BooksStyle>
   );
 };
@@ -48,18 +61,34 @@ const BooksStyle = styled.div`
   width: 100%;
   margin: 0 auto;
   max-width: ${({ theme }) => theme.layout.width.large};
+  height: 100%;
   padding: 2rem;
 
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  gap: 16px;
+  gap: 1rem;
+
+  .main-contents {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 16px;
+  }
 
   .filter {
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 10px 0;
+  }
+
+  .more {
+    margin-top: 2rem;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 export default BooksPage;
